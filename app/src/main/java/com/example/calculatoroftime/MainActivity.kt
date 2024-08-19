5a9738bea579f05380d3a9d7c199ad825fad7d9b
Roman Mokhnatkin <kodiiiofc@gmail.com>
2024-08-19 16:50:55 +0500

    Домашнее задание по теме "Android Calculator"

--- app/src/main/java/com/example/calculatoroftime/MainActivity.kt
+++ app/src/main/java/com/example/calculatoroftime/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.calculatoroftime
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var fromET : EditText
+    private lateinit var toET : EditText
+
+    private lateinit var plusBTN: Button
+    private lateinit var minusBTN: Button
+
+    private lateinit var resultTV: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +29,36 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        fromET = findViewById(R.id.fromET)
+        toET = findViewById(R.id.toET)
+
+        resultTV = findViewById(R.id.resultTV)
+
+        plusBTN = findViewById(R.id.plusBTN)
+        minusBTN = findViewById(R.id.minusBTN)
+
+        plusBTN.setOnClickListener(this)
+        minusBTN.setOnClickListener(this)
+
     }
+
+    override fun onClick(view: View) {
+
+        val timeFrom = TimeCalculator(fromET.text.toString())
+        val timeTo = TimeCalculator(toET.text.toString())
+
+        try {
+            resultTV.text = when (view.id) {
+                R.id.plusBTN -> timeFrom.operation(timeTo, timeTo.sum)
+                R.id.minusBTN -> timeFrom.operation(timeTo, timeTo.dif)
+                else -> "Ошибка вычисления"
+            }
+        } catch (e: Exception) {
+            resultTV.text = "Что-то пошло не так"
+        }
+
+    }
+
+
 }
